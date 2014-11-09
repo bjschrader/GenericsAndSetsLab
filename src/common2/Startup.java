2e656c0df4b1f4be832fe4230fa0f792766fb8ad
schra_000 <schra_000@Brianstorm.attlocal.net>
2014-11-09 10:39:26 -0600

    Initial commit

---
+++ src/common2/Startup.java
@@ -0,0 +1,53 @@
+package common2;
+
+import java.util.*;
+
+public class Startup {
+    
+    
+
+    public static void main(String[] args) {
+        
+        
+        List employeeSSN = new ArrayList<>();
+        
+        employeeSSN.add("123-45-6789");
+        employeeSSN.add("999-99-9999");
+        employeeSSN.add("444-44-4444");
+        employeeSSN.add("123-45-6789");
+       
+        List ssn = new ArrayList<>(employeeSSN);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        Address a1 = new Address("1234 Street", "Milwaukee", "222-222-2222");
+//        Address a2 = new Address("0234 Street", "Milwaukee", "222-222-2222");
+//        Address a3 = new Address("9234 Street", "Milwaukee", "222-222-2222");
+//        Address a4 = new Address("1234 Street", "Milwaukee", "222-222-2222");
+//        
+//        Set set = new HashSet();
+//        set.add(a1);
+//        set.add(a2);
+//        set.add(a3);
+//        set.add(a4);
+//        
+//        for(Object address : set) {
+//            System.out.println(address);
+//        }
+        
+//        System.out.println(a1);
+//        System.out.println(a2);
+
+//        if (a1.equals(a2)) {
+//            System.out.println("The addresses are equal");
+//        } else {
+//            System.out.println("The addresses are NOT equal");
+//        }
+    }
+}
